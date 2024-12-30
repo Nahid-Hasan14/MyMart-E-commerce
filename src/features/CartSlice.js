9455af82778dc691bd3d63268f4e91e11088f2dd
Md Nahid Hasan <hasan141894@gmail.com>
2024-12-30 10:40:57 +0600

    update add to cart

--- src/features/CartSlice.js
+++ src/features/CartSlice.js
@@ -1,7 +1,7 @@
 import { createSlice } from "@reduxjs/toolkit"
 
 const initialState = {
-    carts: JSON.parse(localStorage.getItem("cartData")) || []
+    carts: []
 }
 
 export const cartSlice = createSlice({
